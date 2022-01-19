064a725dd32684d107050c6104a7ecd06ac817da
PKMNRenunciation <yeomansjf@gmail.com>
2022-01-19 08:41:41 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystemV2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HealthSystemV2.0");
+            Console.WriteLine("V2.0 = w/ OOP");
+            Console.WriteLine("");
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HealthSystemV2.0
+ 
